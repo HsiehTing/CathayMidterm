ad93408d12ae560b87648fb7116c5ff422d8975a
HsiehTing <sting20210@gmail.com>
2025-04-28 09:14:02 +0800

    Initial Commit

---
+++ MidtermProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MidtermProject
+//
+//  Created by TWINB00599242 on 2025/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
